0cdc0f90ab0e469d100c3aabc79cb6ce95e5e4cd
MSWS <imodmaker@gmail.com>
2025-03-25 13:09:00 -0700

    fix: Add Restic unlock commands for reliable backups
    Add unlock commands to Restic repositories in backup script
    
    - Add command to unlock the default Restic repository before backup, improving reliability
    - Add command to unlock the USB backup repository to ensure proper access

--- backup.sh
+++ backup.sh
@@ -4,7 +4,9 @@ source /home/iboaz/.restic.sh
 
 exec > >(logger -t backup) 2>&1
 
+/usr/bin/restic unlock
 /usr/bin/restic backup -o s3.storage-class=INTELLIGENT_TIERING --exclude-file=/home/iboaz/excludes.txt --one-file-system --verbose /
+/usr/bin/restic -r /mnt/usb/backups/ unlock
 /usr/bin/restic -r /mnt/usb/backups/ backup --exclude-file=/home/iboaz/excludes.txt --one-file-system --verbose /
 /usr/bin/restic forget --keep-last 1 --keep-daily 3 --keep-weekly 4 --keep-monthly 3 --keep-yearly 3 --prune
 /usr/bin/restic -r /mnt/usb/backups/ forget --keep-last 1 --keep-daily 7 --keep-weekly 8 --keep-monthly 3 --keep-yearly 3 --prune
